8d0634c4ac8d3c6db014529d4be206eed68b4835
DENGy <dengymacias2017@gmail.com>
2026-07-07 12:38:27 -0500

    vista administrativos

---
+++ django/usuarios/views/__init__.py
@@ -0,0 +1,7 @@
+from .v_autenticacion import *
+from .v_estudiantes import *
+from .v_administrativos import *
+from .v_coordinadores_dan import *
+from .v_coordinadores_ua import *
+from .v_docentes import *
+from .v_perfil import *
